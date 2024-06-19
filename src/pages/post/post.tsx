bf5fcf17f2480556adf1c674b03d5b1c94633439
guskirb <anguskirby1998@gmail.com>
2024-06-19 09:58:20 +0100

    Add post page markup & styling

--- src/pages/post/post.tsx
+++ src/pages/post/post.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import unescape from "validator/lib/unescape";
+import "./post.css";
 
 import Spinner from "../../components/spinner/spinner";
 
 export default function Post() {
-  const [post, setPost] = useState('');
+  const [post, setPost] = useState("");
   const [loading, setLoading] = useState(true as boolean);
   let { postId } = useParams();
 
@@ -13,7 +15,7 @@ export default function Post() {
     axios
       .get(`https://blog-api-guskirb.adaptable.app/posts/${postId}`)
       .then((data) => {
-        setPost(data.data);
+        setPost(data.data.post);
         setLoading(false);
       });
   }, []);
@@ -23,8 +25,21 @@ export default function Post() {
   }
 
   return (
-    <div>
-    
+    <div className="post">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${unescape(post.image_url)})`,
+        }}
+      ></div>
+      <div className="title">
+        <h2>{post.title}</h2>
+        <div className="sub_header">
+          <h3>{post.date_formatted}</h3>
+          <h3>BY {post.author.username.toUpperCase()}</h3>
+        </div>
+        <p>{unescape(post.post)}</p>
+      </div>
     </div>
-    );
+  );
 }
