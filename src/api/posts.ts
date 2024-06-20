702cade9acb431468817b12019538380b7114ec3
guskirb <anguskirby1998@gmail.com>
2024-06-20 08:00:21 +0100

    Set up axios base URL

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from "./axios";
 
 export function getPosts() {
   return axios
-    .get("https://blog-api-guskirb.adaptable.app/posts")
+    .get("/posts")
     .then((data) => data.data.posts);
 }
 
 export function getPost(postId) {
   return axios
-    .get(`https://blog-api-guskirb.adaptable.app/posts/${postId}`)
+    .get(`/posts/${postId}`)
     .then((data) => data.data.post);
 }
