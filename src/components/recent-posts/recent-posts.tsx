94b22cc3aece7b30a194235da76498b735e5c166
guskirb <anguskirby1998@gmail.com>
2024-06-23 17:25:31 +0100

    Add recent posts component

--- src/components/recent-posts/recent-posts.tsx
+++ src/components/recent-posts/recent-posts.tsx
@@ -1,15 +1,24 @@
-export default function RecentPosts({posts, postId}) {
-   
-    const listPosts = posts?.filter((post:object) => post._id !== postId)
-    .slice(0, 3).map((post) => (
-        <div key={post._id}>
-            {post.title}
-        </div>
-    ))
+import unescape from "validator/lib/unescape";
+import { Link } from "react-router-dom";
+import "./recent-posts.css";
 
-  return (
-    <div className="recent-posts__container">
-        {listPosts}
-    </div>
-  )
+export default function RecentPosts({ posts, postId }) {
+  const listPosts = posts
+    ?.filter((post: object) => post._id !== postId)
+    .slice(0, 3)
+    .map((post) => (
+      <div className="recent-post" key={post._id}>
+        <Link to={`/post/${post._id}`} key={post._id} state={{ posts: posts }}>
+          <div
+            className="recent-post__image"
+            style={{
+              backgroundImage: `url(${unescape(post.image_url)})`,
+            }}
+          ></div>
+          {post.title}
+        </Link>
+      </div>
+    ));
+
+  return <div className="recent-posts__container">{listPosts}</div>;
 }
